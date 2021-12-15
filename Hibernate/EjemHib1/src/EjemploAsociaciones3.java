7ce590023c5661e7ee2297583f5c0af2c1ff7601
J210172 <J210172@iessierradeguara.com>
2021-12-15 14:18:47 +0100

    .

--- Hibernate/EjemHib1/src/EjemploAsociaciones3.java
+++ Hibernate/EjemHib1/src/EjemploAsociaciones3.java
@@ -26,52 +26,60 @@ public class EjemploAsociaciones3 {
 		SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
 		Session session = sesion.openSession();
 
-		String hql = "from Empleados where oficio = 'DIRECTOR'";
+		String hql = "from Empleados";
 		Query cons = session.createQuery(hql);
 		List<Empleados> empleados = cons.list();
 
-
-		Query cons2 = session.createQuery("from empleados where empno = 1");
-		if (cons.uniqueResult() == null) {
-			
+		Query cons2 = session.createQuery("from Empleados where empNo = :num");
+		cons2.setInteger("num", 1);
+		if (cons2.uniqueResult() == null) {
+			Empleados e = (Empleados) cons2.uniqueResult();		
 		}
 		
-		MyFrame myF = new MyFrame(empleados.stream().map(Empleados::getApellido).toArray(String[]::new));
+		String list1[] = empleados.stream().map(Empleados::getApellido).toArray(String[]::new);
+		Float list2[] = empleados.stream().map(Empleados::getSalario).toArray(Float[]::new);
+		MyFrame myF = new MyFrame(list1, list2);
 //		MyFrame myF = new MyFrame();
 //		empleados.forEach(myF::addItem);
-		
-		session.close();		
+
+		session.close();
 	}
 }
 
 class MyFrame extends JFrame {
 	private JPanel contentPane;
-	private JComboBox<String> myCB;
-	
-	public MyFrame(String[] lista) {
+	private JComboBox<String> myCB1;
+	private JComboBox<Float> myCB2;
+
+	public MyFrame(String[] lista, Float[] listaSal) {
 		setUp();
-		myCB.setModel(new DefaultComboBoxModel<String>(lista));
+		myCB1.setModel(new DefaultComboBoxModel<String>(lista));
+		myCB2.setModel(new DefaultComboBoxModel<Float>(listaSal));
 	}
-	
+
 	public MyFrame() {
 		setUp();
 	}
-	
+
 	void setUp() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(300,300);	
-		
+		setSize(300, 300);
+
 		contentPane = new JPanel();
 		contentPane.setLayout(new FlowLayout());
 		setContentPane(contentPane);
-		
-		myCB = new JComboBox<>();
-		contentPane.add(myCB);
-		
+
+		myCB1 = new JComboBox<>();
+		contentPane.add(myCB1);
+
+		myCB2 = new JComboBox<>();
+		contentPane.add(myCB2);
+
+
 		setVisible(true);
 	}
-	
+
 	public void addItem(Empleados emp) {
-		myCB.addItem(emp.getApellido());
+		myCB1.addItem(emp.getApellido());
 	}
 }
